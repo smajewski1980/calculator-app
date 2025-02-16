d52d1237e4e538b073ab3245e3793b27f7e3f8a3
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-02-16 17:14:01 -0500

    finished js logic

--- index2.js
+++ index2.js
@@ -57,9 +57,38 @@ class Calculator {
     this.previousOperand = "";
   }
 
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
+
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0,
+      });
+    }
+
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+
   updateDisplay() {
-    currentOperandTextElement.innerText = this.currentOperand;
-    previousOperandTextElement.innerText = this.previousOperand;
+    currentOperandTextElement.innerText = this.getDisplayNumber(
+      this.currentOperand
+    );
+    if (this.operation) {
+      previousOperandTextElement.innerText = `${this.getDisplayNumber(
+        this.previousOperand
+      )} ${this.operation}`;
+    } else {
+      previousOperandTextElement.innerText = "";
+    }
   }
 }
 
